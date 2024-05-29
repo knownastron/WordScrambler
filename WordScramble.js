48668cf2a5e09ea5cad2c0f115d361e10129c6f1
Justin Quan <justinquan@Justins-MacBook-Air-3.local>
2024-05-28 22:35:02 -0600

    Revert "fixed bugs"
    revert

--- WordScramble.js
+++ WordScramble.js
@@ -1,15 +1,14 @@
 const words = ["apple", "plane", "orange", "tiger", "soccer", "basketball", "whale", "water", "lebron", "lion", "hippo", "bottle", "kyrie"];
-let random;
+
 function randomWord(){
     const randomIndex = Math.floor(Math.random()* words.length);
-    random = words[randomIndex];
-    
+    let random = words[randomIndex];
+    return random;
 }
 
 
 function scramble(){
-    randomWord();
-    let word = random;
+    let word = randomWord();
     let scrambledLetters;
     const letters = word.split('');
     for(let i = letters.length - 1; i > 0; i--){
@@ -19,35 +18,35 @@ function scramble(){
     scrambledLetters = letters.join('');
     document.getElementById("scrambledword").textContent = scrambledLetters;
 }
-
-let checkButtons = false;
 function result(){
     let guess = document.getElementById("answer").value;
-    let lower = guess.toLowerCase();
-    let outcome = "Wrong!";
-    if(lower == random){
-            outcome = "Correct!";
-            document.getElementById("displayResult").textContent = outcome;
-        }
-        document.getElementById("displayResult").textContent = outcome;
+    let outcome;
 
-    if(checkButtons == false){ 
-
-        const newButton = document.createElement('button');
-        newButton.textContent = 'Play Again';
-        document.body.appendChild(newButton);
-        newButton.addEventListener('click',() =>{
-            navigateToPage();
-        });
-        secondButton = document.createElement('button');
-        secondButton.textContent = 'Exit';
-        document.body.appendChild(secondButton);
-        secondButton.addEventListener('click',() =>{
-            window.location.href = "intro.html";
-        });
-        checkButtons = true;
+    if (words.includes(guess) == true){
+        outcome = "Correct!";
+        document.getElementById("x").textContent = outcome;
+        
     }
+    else{
+        outcome = "Wrong!";
+        document.getElementById("x").textContent = outcome;
+    }
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Play Again';
+    document.body.appendChild(newButton);
+    newButton.addEventListener('click',() =>{
+        navigateToPage();
+    })
+    secondButton = document.createElement('button');
+    secondButton.textContent = 'Exit';
+    document.body.appendChild(secondButton);
+    secondButton.addEventListener('click',() =>{
+        window.location.href = "WordScramble.html";
+    })
+
+
+
 }
 function navigateToPage(){
-    window.location.href = "WordScramble.html";
+    window.location.href = "WordScramble2.html";
 }

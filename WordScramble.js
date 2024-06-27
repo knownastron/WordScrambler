db7d813848049c6370d88f4b54c6359385c7afe1
Justin Quan <justinquan@Justins-MacBook-Air-3.local>
2024-06-26 19:31:28 -0600

    fixed the comments

--- WordScramble.js
+++ WordScramble.js
@@ -1,17 +1,15 @@
 const words = ["apple", "plane", "orange", "tiger", "soccer", "basketball", "whale", "water", "lebron", "lion", "hippo", "bottle", "kyrie"];
-let random;
-function randomWord(){
+let wordToBeGuessed;
+function setWordToBeGuessed(){
     const randomIndex = Math.floor(Math.random()* words.length);
-    random = words[randomIndex];
+    wordToBeGuessed = words[randomIndex];
     
 }
 
-
 function scramble(){
-    randomWord();
-    let word = random;
+    setWordToBeGuessed();
     let scrambledLetters;
-    const letters = word.split('');
+    const letters = wordToBeGuessed.split('');
     for(let i = letters.length - 1; i > 0; i--){
         const randomNum = Math.floor(Math.random() * (i+1));
         [letters[i],letters[randomNum]] = [letters[randomNum], letters[i]];
@@ -20,18 +18,18 @@ function scramble(){
     document.getElementById("scrambledword").textContent = scrambledLetters;
 }
 
-let checkButtons = false;
+let isThereButtons = false;
 function result(){
     let guess = document.getElementById("answer").value;
     let lower = guess.toLowerCase();
     let outcome = "Wrong!";
-    if(lower == random){
+    if(lower == wordToBeGuessed){
             outcome = "Correct!";
             document.getElementById("displayResult").textContent = outcome;
         }
         document.getElementById("displayResult").textContent = outcome;
 
-    if(checkButtons == false){ 
+    if (isThereButtons == false){ 
 
         const newButton = document.createElement('button');
         newButton.textContent = 'Play Again';
@@ -45,7 +43,7 @@ function result(){
         secondButton.addEventListener('click',() =>{
             window.location.href = "intro.html";
         });
-        checkButtons = true;
+        isThereButtons = true;
     }
 }
 function navigateToPage(){

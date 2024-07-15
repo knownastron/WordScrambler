ca8ed9af331395427709df13bc99db6f20f5f321
Justin Quan <justinquan@Justins-MacBook-Air-3.local>
2024-07-15 16:55:45 -0600

    finished my api

--- WordScramble.js
+++ WordScramble.js
@@ -1,4 +1,5 @@
 const words = ["apple", "plane", "orange", "tiger", "soccer", "basketball", "whale", "water", "lebron", "lion", "hippo", "bottle", "kyrie"];
+<<<<<<< Updated upstream
 
 function randomWord(){
     const randomIndex = Math.floor(Math.random()* words.length);
@@ -11,6 +12,19 @@ function scramble(){
     let word = randomWord();
     let scrambledLetters;
     const letters = word.split('');
+=======
+let wordToBeGuessed;
+function setWordToBeGuessed(){
+    const randomIndex = Math.floor(Math.random()* words.length);
+    wordToBeGuessed = words[randomIndex];
+    
+}
+
+function scramble(){
+    setWordToBeGuessed();
+    let scrambledLetters;
+    const letters = wordToBeGuessed.split('');
+>>>>>>> Stashed changes
     for(let i = letters.length - 1; i > 0; i--){
         const randomNum = Math.floor(Math.random() * (i+1));
         [letters[i],letters[randomNum]] = [letters[randomNum], letters[i]];
@@ -18,6 +32,7 @@ function scramble(){
     scrambledLetters = letters.join('');
     document.getElementById("scrambledword").textContent = scrambledLetters;
 }
+<<<<<<< Updated upstream
 function result(){
     let guess = document.getElementById("answer").value;
     let outcome;
@@ -49,4 +64,37 @@ function result(){
 }
 function navigateToPage(){
     window.location.href = "WordScramble2.html";
+=======
+
+let isThereButtons = false;
+function result(){
+    let guess = document.getElementById("answer").value;
+    let lower = guess.toLowerCase();
+    let outcome = "Wrong!";
+    if(lower == wordToBeGuessed){
+            outcome = "Correct!";
+            document.getElementById("displayResult").textContent = outcome;
+        }
+        document.getElementById("displayResult").textContent = outcome;
+
+    if (isThereButtons == false){ 
+
+        const newButton = document.createElement('button');
+        newButton.textContent = 'Play Again';
+        document.body.appendChild(newButton);
+        newButton.addEventListener('click',() =>{
+            navigateToPage();
+        });
+        secondButton = document.createElement('button');
+        secondButton.textContent = 'Exit';
+        document.body.appendChild(secondButton);
+        secondButton.addEventListener('click',() =>{
+            window.location.href = "intro.html";
+        });
+        isThereButtons = true;
+    }
+}
+function navigateToPage(){
+    window.location.href = "WordScramble.html";
+>>>>>>> Stashed changes
 }

e5709e794d82edf55df6846577365b791d944c0a
Justin Quan <justinquan@Justins-MacBook-Air-3.local>
2024-07-16 23:14:55 -0600

    recovered my code

--- WordScramble.js
+++ WordScramble.js
@@ -1,100 +1,54 @@
-const words = ["apple", "plane", "orange", "tiger", "soccer", "basketball", "whale", "water", "lebron", "lion", "hippo", "bottle", "kyrie"];
-<<<<<<< Updated upstream
-
-function randomWord(){
-    const randomIndex = Math.floor(Math.random()* words.length);
-    let random = words[randomIndex];
-    return random;
-}
-
-
-function scramble(){
-    let word = randomWord();
-    let scrambledLetters;
-    const letters = word.split('');
-=======
-let wordToBeGuessed;
-function setWordToBeGuessed(){
-    const randomIndex = Math.floor(Math.random()* words.length);
-    wordToBeGuessed = words[randomIndex];
-    
-}
-
-function scramble(){
-    setWordToBeGuessed();
-    let scrambledLetters;
-    const letters = wordToBeGuessed.split('');
->>>>>>> Stashed changes
-    for(let i = letters.length - 1; i > 0; i--){
-        const randomNum = Math.floor(Math.random() * (i+1));
-        [letters[i],letters[randomNum]] = [letters[randomNum], letters[i]];
+document.addEventListener("keydown", event => {
+    if(event.key == "Enter"){
+        result();
     }
-    scrambledLetters = letters.join('');
-    document.getElementById("scrambledword").textContent = scrambledLetters;
-}
-<<<<<<< Updated upstream
+})
+let isThereButtons = false;
 function result(){
-    let guess = document.getElementById("answer").value;
-    let outcome;
+    let userAnswer = document.getElementById("answer").value;
+    fetch(`http://127.0.0.1:8000/user_answer/${userAnswer}`)
+        .then(response => response.json())
+        .then(data => document.getElementById("displayResult").textContent = data.message)
 
-    if (words.includes(guess) == true){
-        outcome = "Correct!";
-        document.getElementById("x").textContent = outcome;
-        
-    }
-    else{
-        outcome = "Wrong!";
-        document.getElementById("x").textContent = outcome;
-    }
+        if (!isThereButtons){
+            createButtons();
+        }
+}
+
+function createButtons(){
     const newButton = document.createElement('button');
     newButton.textContent = 'Play Again';
     document.body.appendChild(newButton);
     newButton.addEventListener('click',() =>{
         navigateToPage();
-    })
-    secondButton = document.createElement('button');
+    });
+
+    const secondButton = document.createElement('button');
     secondButton.textContent = 'Exit';
     document.body.appendChild(secondButton);
     secondButton.addEventListener('click',() =>{
-        window.location.href = "WordScramble.html";
-    })
-
-
-
-}
-function navigateToPage(){
-    window.location.href = "WordScramble2.html";
-=======
-
-let isThereButtons = false;
-function result(){
-    let guess = document.getElementById("answer").value;
-    let lower = guess.toLowerCase();
-    let outcome = "Wrong!";
-    if(lower == wordToBeGuessed){
-            outcome = "Correct!";
-            document.getElementById("displayResult").textContent = outcome;
-        }
-        document.getElementById("displayResult").textContent = outcome;
-
-    if (isThereButtons == false){ 
-
-        const newButton = document.createElement('button');
-        newButton.textContent = 'Play Again';
-        document.body.appendChild(newButton);
-        newButton.addEventListener('click',() =>{
-            navigateToPage();
-        });
-        secondButton = document.createElement('button');
-        secondButton.textContent = 'Exit';
-        document.body.appendChild(secondButton);
-        secondButton.addEventListener('click',() =>{
-            window.location.href = "intro.html";
-        });
-        isThereButtons = true;
-    }
+    window.location.href = "intro.html";
+    });
+    
+    isThereButtons = true;
+    
 }
 function navigateToPage(){
     window.location.href = "WordScramble.html";
->>>>>>> Stashed changes
+   
+}
+
+function fetchRandomWord(){
+    const randomIndex = Math.floor(Math.random()* 13);
+    fetch(`http://127.0.0.1:8000/scramble/${randomIndex}`)
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Could not fetch resource");
+            }
+            return response.json();
+        })
+        .then(data => document.getElementById("scrambledword").textContent = data.message)
+        .catch(error => console.log(error))
+
+        
 }
